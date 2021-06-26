89babd66a1eeed162f49d274eeefe4a2a6d7e6de
Justin Wu <justinwu820@gmail.com>
2021-06-26 03:32:28 -0400

    Added TODO comments

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -14,6 +14,17 @@ class App extends Component {
   onDragEnd = (result) => {
     // TODO: Structure the component, if user login = render sidepanel + Editable Page
     // TODO: If user not login, show only the demo (editable Page)
+    // TODO: change class to className
+    // * Two solution to structure the component
+    /*
+    1.  Keep Sidepanel as sidepanel component. Pass a bunch of component to show in sidepanel. If user login,pass a bunch
+    2. Make sidepanel no longer a sidepanel but a screen that user login.
+      - Pack everything in one component editpage
+      - Change the name of sidepanel because it's the screenafter user login
+            - Put navbar(sign in button etc) in top of App.js
+      - If(user logined)render the editpage + sidepanel
+      - If(user not login)render the editpage only
+    */
     const { destination, source } = result
 
     // If no destination, exit
