3efbf90e4fda018ae28d82ada7ae267ead07d5f2
Justin Wu <justinwu820@gmail.com>
2021-07-12 22:43:31 -0400

    Set up all the CRUD routes

--- backend/routes/pages.js
+++ backend/routes/pages.js
@@ -1,12 +1,14 @@
 const router = require('express').Router()
 let Page = require('../models/pages.model')
 
+// Return all the page
 router.route('/').get((req, res) => {
   Page.find()
     .then((pages) => res.json(pages))
     .catch((err) => res.status(400).json('Error' + err))
 })
 
+// Add a page
 router.route('/add').post((req, res) => {
   const googleEmail = req.body.googleEmail
   const pageData = req.body.pageData
@@ -16,6 +18,37 @@ router.route('/add').post((req, res) => {
   newPage
     .save()
     .then(() => res.json('Page added!'))
+    .catch((err) => res.status(400).json('Error' + err))
+})
+
+// Return single page data
+router.route('/:id').get((req, res) => {
+  Page.findById(req.params.id)
+    .then((page) => res.json(page))
+    .catch((err) => res.status(400).json('Error' + err))
+})
+
+// Delete a Page
+router.route('/:id').delete((req, res) => {
+  Page.findByIdAndDelete(req.params.id)
+    .then((page) => res.json('Page deleted'))
+    .catch((err) => res.status(400).json('Error' + err))
+})
+
+// Update the data in a page
+router.route('/update/:id').post((req, res) => {
+  Page.findById(req.params.id)
+    .then((page) => {
+      page.pageData = req.body.pageData
+
+      // update &　save it to the database
+      page
+        .save()
+        .then(() => res.json('Page Updated'))
+        .catch((err) => {
+          res.status(400).json('Error' + err)
+        })
+    })
     .catch((err) => res.status(400).json('Error' + err))
 })
 
